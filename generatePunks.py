7d5e934b1e8ea722cc7b157513fab017d3c3d0fb
snoozesecurity <snoozesec@gmail.com>
2022-01-11 21:33:33 -0800

    Modified main function; added most attributes; fixed dir structure

--- generatePunks.py
+++ generatePunks.py
@@ -1,6 +1,7 @@
 from PIL import Image
 import numpy as np
-import hashlib, random
+import hashlib, os, random
+import probability as prob
 
 # Keep track of punks created
 punksCreated = 0
@@ -12,148 +13,95 @@ fileHashes = []
 punks = ['punk01.png', 'punk02.png', 'punk03.png', 'punk04.png', 'punk05.png', 'punk06.png', 'punk07.png', 'punk08.png', 'punk09.png', 'punk10.png', 'punk11.png']
 punkTypes = ['male', 'female', 'alien', 'ape', 'zombie']
 
-# Male attributes
-head = ['hair01.png', 'hair02.png', 'hair03.png', 'hat01.png', 'hoodie.png']
-glasses = ['glasses01.png', 'glasses02.png', 'glasses03.png', 'glasses04.png']
-facialHair = ['beard01.png', 'beard02.png']
-mask = 'mask01.png'
-
-# Female attributes
-femaleHead = ['femalehair01.png', 'femalehair02.png', 'femalehair03.png', 'femalehair04.png', 'femalehair05.png', 'femalehair06.png', 'femalehair07.png', 'femaletiara01.png', 'femalehat01.png']
-femaleEyewear = ['femaleglasses01.png', 'femaleglasses02.png', 'femaleglasses03.png', 'femaleshadow01.png', 'femaleshadow02.png']
-femaleMask = 'femalemask01.png'
-
-# Ape attributes
-apeHead = ['hat01.png', 'hat02.png', 'hat03.png', 'hat04.png', 'hoodie.png', 'headband01.png']
-apeGlasses = ['glasses01.png', 'glasses02.png', 'glasses03.png', 'glass04.png']
-
-# Universal attributes
 backgrounds = ['bg01.png', 'bg02.png', 'bg03.png']
 smokes = ['smoke01.png', 'smoke02.png', 'smoke03.png']
-other = ['earring01.png']
 
-def generateMale():
-	punkStack = Image.open(f"punks/{random.choice(punks[0:4])}")
+def generatePunk(punkType):
+	# This can probably be shortened using dynamic variable names or similar
+	if punkType == 'male':
+		attrDict = prob.maleAttr
+		punkStack = Image.open(f"punks/{random.choice(punks[0:4])}")
+	elif punkType == 'female':
+		attrDict = prob.femaleAttr
+		punkStack = Image.open(f"punks/{random.choice(punks[4:8])}")
+	elif punkType == 'alien':
+		attrDict = prob.alienAttr
+		punkStack = Image.open(f"punks/{punks[8]}")
+	elif punkType == 'ape':
+		attrDict = prob.apeAttr
+		punkStack = Image.open(f"punks/{punks[9]}")
+	elif punkType == 'zombie':
+		attrDict = prob.zombieAttr
+		punkStack = Image.open(f"punks/{punks[10]}")
+
+	# Currently head/facialhair/eyes/mouth attributes work for all punk types.
+	# Time to add next attribute; check with
+	# print(sum(list(attrDict['ATTRIBUTETYPE'].values())))
+	# To ensure probability = 1
+
+	# FIX: robber mask needs to cover ear
+	# Mouth modifiers probably shouldn't work if punk has facial hair?
+	attributeCount = 0
+	basedir = f"attributes/{punkType}/"
 	hasHeadAttr = np.random.choice([True, False], p=[0.7, 0.3])
 	if hasHeadAttr:
-		headChoice = Image.open(f"attributes/{np.random.choice(head, p=[0.4, 0.15, 0.2, 0.15, 0.1])}")
+		headAttrs = [f"{basedir}head/{item[0]}" for item in attrDict['head'].items()]
+		headChoice = Image.open(np.random.choice(headAttrs, p=list(attrDict['head'].values())))
 		punkStack.paste(headChoice, (0, 0), headChoice)
+		attributeCount += 1
+	if punkType == 'male' or punkType == 'zombie':
+		hasFacialHair = np.random.choice([True, False], p=[0.3, 0.7])
+		if hasFacialHair:
+			facialHairAttrs = [f"{basedir}facialhair/{item[0]}" for item in attrDict['facialhair'].items()]
+			facialHairChoice = Image.open(np.random.choice(facialHairAttrs, p=list(attrDict['facialhair'].values())))
+			punkStack.paste(facialHairChoice, (0, 0), facialHairChoice)
+			attributeCount += 1
 	hasGlasses = np.random.choice([True, False], p=[0.7, 0.3])
 	if hasGlasses:
-		glassesChoice = Image.open(f"attributes/{np.random.choice(glasses, p=[0.4, 0.4, 0.1, 0.1])}")
-		punkStack.paste(glassesChoice, (0, 0), glassesChoice)
-	hasFacialHair = np.random.choice([True, False], p=[0.25, 0.75])
-	if hasFacialHair:
-		facialHairChoice = Image.open(f"attributes/{np.random.choice(facialHair, p=[0.5, 0.5])}")
-		punkStack.paste(facialHairChoice, (0, 0), facialHairChoice)
-	hasSmoke = np.random.choice([True, False], p=[0.15, 0.85])
-	if hasSmoke:
-		smokeChoice = Image.open(f"attributes/{np.random.choice(smokes, p=[0.3, 0.3, 0.4])}")
-		punkStack.paste(smokeChoice, (0, 0), smokeChoice)
-	hasMask = np.random.choice([True, False], p=[0.02, 0.98])
-	if hasMask:
-		maleMask = Image.open(f"attributes/{mask}")
-		punkStack.paste(maleMask, (0, 0), maleMask)
-	elif not hasMask:
-		hasSmoke = np.random.choice([True, False], p=[0.35, 0.65])
+		glassesAttrs = [f"{basedir}eyes/{item[0]}" for item in attrDict['eyes'].items()]
+		if glassesAttrs:
+			glassesChoice = Image.open(np.random.choice(glassesAttrs, p=list(attrDict['eyes'].values())))
+			punkStack.paste(glassesChoice, (0, 0), glassesChoice)
+			attributeCount += 1
+	hasMouthModifier = np.random.choice([True, False], p=[0.6, 0.4])
+	if hasMouthModifier:
+		mouthAttrs = [f"{basedir}mouth/{item[0]}" for item in attrDict['mouth'].items()]
+		if mouthAttrs:
+			mouthChoice = Image.open(np.random.choice(mouthAttrs, p=list(attrDict['mouth'].values())))
+			punkStack.paste(mouthChoice, (0, 0), mouthChoice)
+			attributeCount += 1
+	hasMask = np.random.choice([True, False], p=[0.025, 0.975])
+	if not hasMask:
+		hasSmoke = np.random.choice([True, False], p=[.25, .75])
 		if hasSmoke:
-			smokeChoice = Image.open(f"attributes/{np.random.choice(smokes, p=[0.3, 0.3, 0.4])}")
-			punkStack.paste(smokeChoice, (0,0), smokeChoice)
-	return punkStack
-
-def generateFemale():
-	punkStack = Image.open(f"punks/{random.choice(punks[4:8])}")
-	hasHeadAttr = np.random.choice([True, False], p=[0.8, 0.2])
-	if hasHeadAttr:
-		headChoice = Image.open(f"attributes/{np.random.choice(femaleHead, p=[0.15, 0.15, 0.15, 0.15, 0.15, 0.075, 0.075, 0.025, 0.075])}")
-		punkStack.paste(headChoice, (0, 0), headChoice)
-	hasEyewear = np.random.choice([True, False], p=[0.8, 0.2])
-	if hasEyewear:
-		eyewearChoice = Image.open(f"attributes/{np.random.choice(femaleEyewear, p=[0.2, 0.2, 0.2, 0.2, 0.2])}")
-		punkStack.paste(eyewearChoice, (0, 0), eyewearChoice)
-	hasMask = np.random.choice([True, False], p=[0.02, 0.98])
-	if hasMask:
-		fMask = Image.open(f"attributes/{femaleMask}")
-		punkStack.paste(fMask, (0, 0), fMask)
-	elif not hasMask:
-		hasSmoke = np.random.choice([True, False], p=[0.35, 0.65])
-		if hasSmoke:
-			smokeChoice = Image.open(f"attributes/{np.random.choice(smokes, p=[0.3, 0.3, 0.4])}")
-			punkStack.paste(smokeChoice, (0,0), smokeChoice)
-	return punkStack
-
-def generateAlien():
-	punkStack = Image.open(f"punks/{punks[8]}")
-	hasHeadAttr = np.random.choice([True, False], p=[0.9, 0.1])
-	if hasHeadAttr:
-		headChoice = Image.open(f"attributes/{np.random.choice(head, p=[0.4, 0.15, 0.2, 0.15, 0.1])}")
-		punkStack.paste(headChoice, (0, 0), headChoice)
-	hasGlasses = np.random.choice([True, False], p=[0.7, 0.3])
-	if hasGlasses:
-		glassesChoice = Image.open(f"attributes/{np.random.choice(glasses, p=[0.4, 0.4, 0.1, 0.1])}")
-		punkStack.paste(glassesChoice, (0, 0), glassesChoice)
-	hasMask = np.random.choice([True, False], p=[0.02, 0.98])
-	if hasMask:
-			maleMask = Image.open(f"attributes/{mask}")
-			punkStack.paste(maleMask, (0, 0), maleMask)
-	elif not hasMask:
-		hasSmoke = np.random.choice([True, False], p=[0.35, 0.65])
-		if hasSmoke:
-			smokeChoice = Image.open(f"attributes/{np.random.choice(smokes, p=[0.3, 0.3, 0.4])}")
-			punkStack.paste(smokeChoice, (0,0), smokeChoice)
-	return punkStack
-
-def generateApe():
-	punkStack = Image.open(f"punks/{punks[9]}")
-	headChoice = Image.open(f"attributes/{np.random.choice(apeHead, p=[0.3, 0.1, 0.15, 0.2, 0.15, 0.1])}")
-	punkStack.paste(headChoice, (0, 0), headChoice)
-	hasGlasses = np.random.choice([True, False], p=[0.7, 0.3])
-	if hasGlasses:
-		glassesChoice = Image.open(f"attributes/{np.random.choice(glasses, p=[0.35, 0.35, 0.15, 0.15])}")
-		punkStack.paste(glassesChoice, (0, 0), glassesChoice)
-	hasSmoke = np.random.choice([True, False], p=[0.15, 0.85])
-	if hasSmoke:
-		smokeChoice = Image.open(f"attributes/{np.random.choice(smokes, p=[0.3, 0.3, 0.4])}")
-		punkStack.paste(smokeChoice, (0,0), smokeChoice)
-	return punkStack
-
-def generateZombie():
-	punkStack = Image.open(f"punks/{punks[10]}")
-	hasHeadAttr = np.random.choice([True, False], p=[0.5, 0.5])
-	if hasHeadAttr:
-		headChoice = Image.open(f"attributes/{np.random.choice(head, p=[0.4, 0.15, 0.2, 0.15, 0.1])}")
-		punkStack.paste(headChoice, (0, 0), headChoice)
-	hasGlasses = np.random.choice([True, False], p=[0.7, 0.3])
-	if hasGlasses:
-		glassesChoice = Image.open(f"attributes/{np.random.choice(glasses, p=[0.25, 0.25, 0.25, 0.25])}")
-		punkStack.paste(glassesChoice, (0, 0), glassesChoice)
-	hasFacialHair = np.random.choice([True, False], p=[0.25, 0.75])
-	if hasFacialHair:
-		facialHairChoice = Image.open(f"attributes/{np.random.choice(facialHair, p=[0.5, 0.5])}")
-		punkStack.paste(facialHairChoice, (0, 0), facialHairChoice)
-	hasMask = np.random.choice([True, False], p=[0.02, 0.98])
-	if hasMask:
-		maleMask = Image.open(f"attributes/{mask}")
-		punkStack.paste(maleMask, (0, 0), maleMask)
+			smokeChoice = Image.open(f"attributes/uni/smoke/{np.random.choice(smokes, p=[0.33, 0.33, 0.34])}")
+			punkStack.paste(smokeChoice, (0, 0), smokeChoice)
+			attributeCount += 1
+	if hasMask and punkType != 'ape':
+		maskChoice = Image.open(f"{basedir}mask/mask01.png")
+		punkStack.paste(maskChoice, (0, 0), maskChoice)
+		attributeCount += 1
+	hasEarring = np.random.choice([True, False], p=[0.15, 0.85])
+	if hasEarring:
+		earringChoice = Image.open(f"{basedir}earring/earring01.png")
+		punkStack.paste(earringChoice, (0, 0), earringChoice)
+		attributeCount += 1
+	hasNecklace = np.random.choice([True, False], p=[0.2, 0.8])
+	if hasNecklace:
+		neckAttrs = [f"{basedir}neck/{item[0]}" for item in attrDict['neck'].items()]
+		if neckAttrs:
+			neckChoice = Image.open(np.random.choice(neckAttrs, p=list(attrDict['neck'].values())))
+			punkStack.paste(neckChoice, (0, 0), neckChoice)
+			attributeCount += 1
+	if attributeCount > 6:
+		print(f"Creating {punkType} with {attributeCount} attributes")
 	return punkStack
 
 # While loop for total number of punks to be generated
-while punksCreated < 100:
+while punksCreated < 10000:
 
 	# Select punk and start stacking randomly chosen layers using the appropriate function
-	punkType = np.random.choice(punkTypes, p=[0.5, 0.3, 0.05, 0.06, 0.09])
-
-	# Function call depending on punkType
-	if punkType == 'male':
-		output = generateMale()
-	elif punkType == 'female':
-		output = generateFemale()
-	elif punkType == 'alien':
-		output = generateAlien()
-	elif punkType == 'ape':
-		output = generateApe()
-	elif punkType == 'zombie':
-		output = generateZombie()
+	output = generatePunk(np.random.choice(punkTypes, p=[0.5, 0.3, 0.05, 0.06, 0.09]))
 
 	fileHash = hashlib.md5(output.tobytes())
 	hashDigest = fileHash.hexdigest()
